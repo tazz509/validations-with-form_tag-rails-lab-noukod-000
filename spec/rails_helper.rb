188d1d6881988d9b82e42685c6e5b9b440bd27d7
carly <carly@noukod.com>
2019-01-28 18:04:24 +0000

    Automatically backed up by Learn

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,7 @@
 # require 'rspec/rails'
 # # Add additional requires below this line. Rails is not loaded until this point!
 # require 'capybara/rails'
-# 
+#
 # # Requires supporting ruby files with custom matchers and macros, etc, in
 # # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # # run as spec files by default. This means that files in spec/support that end
@@ -22,20 +22,20 @@
 # # require only the support files necessary.
 # #
 # # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-# 
+#
 # # Checks for pending migration and applies them before tests are run.
 # # If you are not using ActiveRecord, you can remove this line.
 # ActiveRecord::Migration.maintain_test_schema!
-# 
+#
 # RSpec.configure do |config|
 #   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
 #   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-# 
+#
 #   # If you're not using ActiveRecord, or you'd prefer not to run each of your
 #   # examples within a transaction, remove the following line or assign false
 #   # instead of true.
 #   config.use_transactional_fixtures = true
-# 
+#
 #   # RSpec Rails can automatically mix in different behaviours to your tests
 #   # based on their file location, for example enabling you to call `get` and
 #   # `post` in specs under `spec/controllers`.
@@ -50,7 +50,7 @@
 #   # The different available types are documented in the features, such as in
 #   # https://relishapp.com/rspec/rspec-rails/docs
 #   config.infer_spec_type_from_file_location!
-# 
+#
 #   # Filter lines from Rails gems in backtraces.
 #   config.filter_rails_from_backtrace!
 #   # arbitrary gems may also be filtered via:
